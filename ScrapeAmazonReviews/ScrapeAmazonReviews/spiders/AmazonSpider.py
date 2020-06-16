099342dd84b8b7a679c81606d56a9b2bcb4d5ece
Shubham <shubham.adep@wsu.edu>
2020-06-15 17:25:12 -0700

    Working API ( locally ), accepts Seller ID, will send raw data as response

---
+++ ScrapeAmazonReviews/ScrapeAmazonReviews/spiders/AmazonSpider.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from ..items import ScrapeamazonreviewsItem
+
+class AmazonspiderSpider(scrapy.Spider):
+
+    def __init__(self, url=None, *args, **kwargs):
+        
+        '''
+            url as a parameter receives SellerID.
+        '''
+
+        self.Amazon_Standard_Identification_Number = url
+        print(self.Amazon_Standard_Identification_Number)
+        self.page_number = 1
+        # self.start_urls = ['https://www.amazon.com/Echo-Wall-Clock-requires-compatible/product-reviews/'+self.Amazon_Standard_Identification_Number+'/ref=cm_cr_getr_d_paging_btm_next_2?ie=UTF8&reviewerType=all_reviews&pageNumber='+str(1)]
+        self.start_urls = ['https://www.amazon.com/s?me='+url+'&marketplaceID=ATVPDKIKX0DER']
+        self.name = 'AmazonSpider'
+
+
+    def parse(self, response):
+        print('parser is called.')
+        items = ScrapeamazonreviewsItem()
+
+        productInfo = response.css('.a-text-normal span::text').extract()
+        productPrices = response.css('.a-price-whole span::text').extract()
+
+        items['productInfo'] = productInfo
+        items['productPrices'] = productPrices
+        #
+        yield items
+
+        # next_page = 'https://www.amazon.com/Echo-Wall-Clock-requires-compatible/product-reviews/'+self.Amazon_Standard_Identification_Number+'/ref=cm_cr_getr_d_paging_btm_next_2?ie=UTF8&reviewerType=all_reviews&pageNumber='+str(self.page_number)
+        # if self.page_number <= 3:
+        #     self.page_number += 1
+        #     yield response.follow(next_page, callback = self.parse)
---
+++ app_test_scrapping.py
@@ -0,0 +1,64 @@
+import crochet
+crochet.setup()
+
+import flask
+from flask import request
+from scrapy.crawler import CrawlerProcess
+from scrapy.signalmanager import dispatcher
+from scrapy import signals
+from scrapy.utils.project import get_project_settings
+
+from ScrapeAmazonReviews.ScrapeAmazonReviews.spiders import AmazonSpider
+
+
+crawl_runner = CrawlerProcess({
+   'ROBOTSTXT_OBEY': True,
+   'ROTATING_PROXY_LIST_PATH': './proxies.txt',
+   'DOWNLOADER_MIDDLEWARES': {
+       'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+       'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+       'scrapy_proxy_pool.middlewares.ProxyPoolMiddleware': 610,
+       'scrapy_proxy_pool.middlewares.BanDetectionMiddleware': 620,
+   }})
+
+output_data = []
+app = flask.Flask(__name__)
+
+@crochet.wait_for(timeout=60.0)
+def scrape_amazon_reviews(sellerID):
+    dispatcher.connect(_crawler_result, signal=signals.item_scraped)
+    eventual = crawl_runner.crawl(AmazonSpider.AmazonspiderSpider, url=sellerID)
+    return eventual
+
+def _crawler_result(item, response, spider):
+    output_data.append(dict(item))
+
+def formatting_reviews(reviews):
+    corpus = []
+    for paginatedReviews in reviews:
+        for review in paginatedReviews['productInfo']:
+            if review != '\n            ':
+                corpus.append(review)
+    return corpus
+
+@app.route("/scrape", methods=["GET", "POST"])
+def predict():
+    
+    args = request.args
+    sellerID = args["SellerID"]
+    print ("SELLER ID: ", args["SellerID"])
+
+    data = {"success": False}
+
+    scrape_amazon_reviews(sellerID)
+    formatted_output = formatting_reviews(output_data)
+    print("output: ",formatted_output)
+
+    data["ProductInfo"] = formatted_output
+
+    response = flask.jsonify(data)
+
+    return response
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5500)
